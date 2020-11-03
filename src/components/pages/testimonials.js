c4e5116e4e2986fa16f48c274ab8f8b944a6e697
Marek Matejovic <me@devmaroy.com>
2020-11-03 16:18:09 +0100

    Added slider to testimonials

--- src/components/pages/testimonials.js
+++ src/components/pages/testimonials.js
@@ -1,8 +1,22 @@
+/* eslint-disable react/jsx-props-no-spreading */
 import React from 'react';
+import Slider from 'react-slick';
 import Testimonial from '../common/testimonial';
 import testimonialImage01 from '../../images/pages/testimonials/testimonial-01.jpg';
 
 const Testimonials = () => {
+  const settings = {
+    dots: true,
+    arrows: false,
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    // autoplaySpeed: 6000,
+    autoplaySpeed: 6000000,
+    pauseOnHover: true,
+  };
+
   return (
     <section className="testimonials">
       <div className="container">
@@ -12,17 +26,43 @@ const Testimonials = () => {
           </h2>
 
           <div className="testimonials__list">
-            <Testimonial
-              authorImage={testimonialImage01}
-              authorName="Debbie W. Stephens"
-              authorPosition="Freelancer"
-            >
-              <p>
-                ”Cupcake cake sesame snaps lollipop powder cupcake jelly
-                marzipan. Halvah pie dragée carrot cake pudding chocolate.
-                Pastry gummies pastry marshmallow cookie”
-              </p>
-            </Testimonial>
+            <Slider {...settings}>
+              <Testimonial
+                authorImage={testimonialImage01}
+                authorName="Debbie W. Stephens"
+                authorPosition="Freelancer"
+              >
+                <p>
+                  ”Cupcake cake sesame snaps lollipop powder cupcake jelly
+                  marzipan. Halvah pie dragée carrot cake pudding chocolate.
+                  Pastry gummies pastry marshmallow cookie”
+                </p>
+              </Testimonial>
+
+              <Testimonial
+                authorImage={testimonialImage01}
+                authorName="Debbie W. Stephens"
+                authorPosition="Freelancer"
+              >
+                <p>
+                  ”Cupcake cake sesame snaps lollipop powder cupcake jelly
+                  marzipan. Halvah pie dragée carrot cake pudding chocolate.
+                  Pastry gummies pastry marshmallow cookie”
+                </p>
+              </Testimonial>
+
+              <Testimonial
+                authorImage={testimonialImage01}
+                authorName="Debbie W. Stephens"
+                authorPosition="Freelancer"
+              >
+                <p>
+                  ”Cupcake cake sesame snaps lollipop powder cupcake jelly
+                  marzipan. Halvah pie dragée carrot cake pudding chocolate.
+                  Pastry gummies pastry marshmallow cookie”
+                </p>
+              </Testimonial>
+            </Slider>
           </div>
         </div>
       </div>
