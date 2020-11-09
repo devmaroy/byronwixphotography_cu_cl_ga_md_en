1eb2d8c983c665024a1413360479aedb27c170f7
Marek Matejovic <me@devmaroy.com>
2020-11-09 13:49:47 +0100

    Added responsivity for about and fixed hero

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,11 +1,36 @@
 import React from 'react';
+import { graphql, useStaticQuery } from 'gatsby';
+import Img from 'gatsby-image';
 import Layout from '../components/layout/layout';
 import SEO from '../components/common/seo';
 import SubpageHeader from '../components/common/subpageHeader';
 import Social from '../components/common/social';
-import aboutPhoto from '../images/pages/about/about.jpg';
+
+// Query
+const query = graphql`
+  {
+    aboutPhoto: allFile(
+      filter: {
+        sourceInstanceName: { eq: "images" }
+        relativeDirectory: { eq: "subpages/about" }
+        name: { eq: "about" }
+      }
+    ) {
+      nodes {
+        childImageSharp {
+          fluid {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
+    }
+  }
+`;
 
 const About = () => {
+  const data = useStaticQuery(query);
+  const aboutPhoto = data.aboutPhoto.nodes[0].childImageSharp;
+
   return (
     <Layout>
       <SEO
@@ -25,42 +50,52 @@ const About = () => {
               </p>
             </SubpageHeader>
 
-            <div className="about__photo">
-              <img src={aboutPhoto} alt="Byron Wix portrait" />
+            <div className="about__wrapper">
+              <div className="about__info">
+                <div className="about__photo">
+                  <Img
+                    fluid={aboutPhoto.fluid}
+                    className="about__image"
+                    alt="Byron Wix portrait"
+                  />
+                </div>
+
+                <div className="about__social">
+                  <Social />
+                </div>
+              </div>
+
+              <div className="about__content">
+                <div className="about__text">
+                  <h2>
+                    Lemon drops tootsie roll brownie lemon drops. Sweet roll
+                    gingerbread.
+                  </h2>
+
+                  <p>
+                    Chocolate cake tart halvah caramels wafer chocolate sugar
+                    plum jelly beans candy. Ice cream toffee tiramisu bear claw
+                    sweet. Gummies cake icing chocolate bar tart jujubes topping
+                    sugar plum soufflé.
+                  </p>
+
+                  <p>
+                    Pastry danish powder muffin candy cookie. Biscuit muffin oat
+                    cake candy canes dragée tart cupcake marzipan. Pudding
+                    fruitcake brownie cupcake marshmallow jelly-o danish. Bonbon
+                    sugar plum tart sweet sweet roll cotton candy cupcake.
+                    Chocolate cookie cupcake toffee donut dessert.
+                  </p>
+                </div>
+
+                <button
+                  className="button button--secondary about__cta"
+                  type="button"
+                >
+                  Download CV
+                </button>
+              </div>
             </div>
-
-            <div className="about__social">
-              <Social />
-            </div>
-
-            <div className="about__text">
-              <h2>
-                Lemon drops tootsie roll brownie lemon drops. Sweet roll
-                gingerbread.
-              </h2>
-
-              <p>
-                Chocolate cake tart halvah caramels wafer chocolate sugar plum
-                jelly beans candy. Ice cream toffee tiramisu bear claw sweet.
-                Gummies cake icing chocolate bar tart jujubes topping sugar plum
-                soufflé.
-              </p>
-
-              <p>
-                Pastry danish powder muffin candy cookie. Biscuit muffin oat
-                cake candy canes dragée tart cupcake marzipan. Pudding fruitcake
-                brownie cupcake marshmallow jelly-o danish. Bonbon sugar plum
-                tart sweet sweet roll cotton candy cupcake. Chocolate cookie
-                cupcake toffee donut dessert.
-              </p>
-            </div>
-
-            <button
-              className="button button--secondary about__cta"
-              type="button"
-            >
-              Download CV
-            </button>
           </div>
         </div>
       </section>
