5b81e644389b9977fb3b225d0d3afb4feb319caa
Marek Matejovic <me@devmaroy.com>
2021-01-06 20:05:19 +0100

    Dynamic prefooter

--- src/components/layout/preFooter.js
+++ src/components/layout/preFooter.js
@@ -1,22 +1,63 @@
 import React from 'react';
+import { graphql, useStaticQuery } from 'gatsby';
 import Social from '../common/social';
 
+// Query
+const query = graphql`
+  query PreFooterQuery {
+    preFooter: allFile(
+      filter: {
+        internal: { mediaType: { eq: "text/markdown" } }
+        sourceInstanceName: { eq: "markdown-data" }
+        relativeDirectory: { regex: "/prefooter/" }
+        base: { regex: "/index/" }
+      }
+    ) {
+      nodes {
+        childMarkdownRemark {
+          frontmatter {
+            id
+            heading
+            newsletterHeading
+            newsletterFormField {
+              id
+              placeholder
+              type
+              name
+            }
+            newsletterFormButtonText
+          }
+          html
+        }
+      }
+    }
+  }
+`;
+
 const PreFooter = () => {
+  const data = useStaticQuery(query);
+  const preFooterData = data.preFooter.nodes[0].childMarkdownRemark;
+
   return (
     <div className="pre-footer">
       <div className="container">
         <div className="pre-footer__inner">
           <div className="pre-footer__info">
-            <h3 className="pre-footer__heading">
-              Topping pastry cheesecake pastry
-            </h3>
+            <h3
+              className="pre-footer__heading"
+              // eslint-disable-next-line react/no-danger
+              dangerouslySetInnerHTML={{
+                __html: preFooterData.frontmatter.heading,
+              }}
+            />
 
-            <div className="pre-footer__text">
-              <p>
-                Donut pie sweet roll sugar plum carrot cake sweet. Chupa chups
-                donut danish icing pastry pudding sweet ice cream cheesecake.
-              </p>
-            </div>
+            <div
+              className="pre-footer__text"
+              // eslint-disable-next-line react/no-danger
+              dangerouslySetInnerHTML={{
+                __html: preFooterData.html,
+              }}
+            />
 
             <div className="pre-footer__social">
               <Social type="square" />
@@ -24,13 +65,21 @@ const PreFooter = () => {
           </div>
 
           <div className="pre-footer__newsletter">
-            <h3 className="pre-footer__heading">Subscribe to newsletter</h3>
+            <h3
+              className="pre-footer__heading"
+              // eslint-disable-next-line react/no-danger
+              dangerouslySetInnerHTML={{
+                __html: preFooterData.frontmatter.newsletterHeading,
+              }}
+            />
 
             <form className="pre-footer-form">
               <input
-                type="email"
-                name="email"
-                placeholder="Enter your email"
+                type={preFooterData.frontmatter.newsletterFormField.type}
+                name={preFooterData.frontmatter.newsletterFormField.name}
+                placeholder={
+                  preFooterData.frontmatter.newsletterFormField.placeholder
+                }
                 className="pre-footer-form__control"
               />
 
@@ -39,7 +88,7 @@ const PreFooter = () => {
                   type="button"
                   className="button button--sm button--primary pre-footer-form__button"
                 >
-                  Send
+                  {preFooterData.frontmatter.newsletterFormButtonText}
                 </button>
               </div>
             </form>
---
+++ src/markdown-data/prefooter/index.md
@@ -0,0 +1,16 @@
+---
+id: '0ad2f599-82fc-49af-a58c-45d625593624'
+heading: 'Topping pastry cheesecake pastry'
+newsletterHeading: 'Subscribe to newsletter'
+newsletterFormField:
+  {
+    id: '138ad89f-a2c0-44b2-923c-683f5b647e06',
+    placeholder: 'Enter your email',
+    type: 'email',
+    name: 'email',
+  }
+newsletterFormButtonText: 'Send'
+---
+
+Donut pie sweet roll sugar plum carrot cake sweet. Chupa chups donut danish
+icing pastry pudding sweet ice cream cheesecake.
